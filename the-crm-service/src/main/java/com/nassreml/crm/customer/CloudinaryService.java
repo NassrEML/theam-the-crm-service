efac6d50a2c83bdd28329816f6e86c07b29feb7c
NassrEML <nassr906@gmail.com>
2022-01-10 23:50:58 +0000

    Extract hardcode strings to application properties

--- the-crm-service/src/main/java/com/nassreml/crm/customer/CloudinaryService.java
+++ the-crm-service/src/main/java/com/nassreml/crm/customer/CloudinaryService.java
@@ -2,6 +2,7 @@ package com.nassreml.crm.customer;
 
 import com.cloudinary.*;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,21 +13,20 @@ import java.util.Map;
 
 @Service
 public class CloudinaryService {
-    private final String CLOUD_NAME = "";
-    private final String API_KEY = "";
-    private final String API_SECRET = "";
+
+    @Value("${com.nassreml.cloudinary.cloud-name}")
+    private String CLOUD_NAME;
+    @Value("${com.nassreml.cloudinary.api-key}")
+    private String API_KEY;
+    @Value("${com.nassreml.cloudinary.api-secret}")
+    private String API_SECRET;
 
     private Cloudinary cloudinary;
 
-    public CloudinaryService() {
-        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", CLOUD_NAME,
-                "api_key", API_KEY,
-                "api_secret", API_SECRET,
-                "secure", true));
-    }
+    public CloudinaryService() {}
 
     public String uploadFile(final MultipartFile file) {
+        setupCloudinaryVariables();
         try {
             File uploadedFile = convertMultiPartToFile(file);
             Map uploadResult = cloudinary.uploader().upload(uploadedFile, ObjectUtils.emptyMap());
@@ -45,4 +45,13 @@ public class CloudinaryService {
         fos.close();
         return convFile;
     }
+
+    private void setupCloudinaryVariables() {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", CLOUD_NAME,
+                "api_key", API_KEY,
+                "api_secret", API_SECRET,
+                "secure", true));
+    }
+
 }
